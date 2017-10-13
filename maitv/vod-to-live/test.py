eada53c9e587d57b79bacd315c25d21f12a323ef
mariliab <marilia.bognandi@gmail.com>
2017-10-13 09:50:35 +0200

    2 segments from each bitrate each time get_segment() is called
    2 segments from each bitrate each time get_segment() is called

--- maitv/vod-to-live/test.py
+++ maitv/vod-to-live/test.py
@@ -1,26 +1,40 @@
 from vodtolive import HLSVod
+import threading
 
 def main():
-	vod = HLSVod('http://dw2nch8cl472k.cloudfront.net/HLS/Apple%20HLS/HTTP%20example.m3u8')
-	#vod.dump()
-	#print "LIST PLAYLISTS"
-	#vod.list_playlists()
-	#print('HLS START')
-
-
-	print "GET SEGMENT for bitrate 18830456"
-
-	print vod.get_segment('18830456')
-
-	print vod.get_segment('18830456')
-
-	print vod.get_segment('18830456')
-
-	print vod.get_segment('18830456')
-
+    
+    vod = HLSVod('http://dw2nch8cl472k.cloudfront.net/HLS/Apple%20HLS/HTTP%20example.m3u8')
+        #vod.dump()
+        #print "LIST PLAYLISTS"
+        #vod.list_playlists()
+        #print('HLS START')
+        playIsActive = True
+        
+        def timerFunction():
+            timer = threading.Timer(3.0, timerFunction)
+                if playIsActive == True:
+                    timer.start()
+                        #print vod.get_segment('18830456')
+                        
+                        for bw in vod.m3u8_obj.playlists:
+                            bandwidths = []
+                                bw = bw.stream_info.bandwidth
+                                bandwidths.append(bw)
+                                #print("Bandwidth")
+                                #print bw
+                                for x in bandwidths:
+                                    print "2 SEGMENTS with bandwidth " + str(x)
+                                        #print x
+                                        print vod.get_segment(str(x))
+                        #print vod.get_segment('18830456')
+                    vod.next()
+            
+                else:
+                    timer.cancel()
+                        timerFunction()
 
 if __name__ == '__main__':
-	try:
-		main()
-	except Exception, err:
-		raise
+    try:
+        main()
+        except Exception, err:
+        raise
--- maitv/vod-to-live/vodtolive/__init__.py
+++ maitv/vod-to-live/vodtolive/__init__.py
@@ -2,61 +2,61 @@ import m3u8
 
 
 class HLSVod:
-	def __init__(self, hlsManifestUri):
-		self.hlsManifestUri = hlsManifestUri
-		self.m3u8_obj = m3u8.load(self.hlsManifestUri)
-		self.segments = {}
+    def __init__(self, hlsManifestUri):
+        self.hlsManifestUri = hlsManifestUri
+            self.m3u8_obj = m3u8.load(self.hlsManifestUri)
+                self.segments = {}
+                
+                #print "HLSVOD Object"
+                #print self.m3u8_obj
+                
+                print "BASE URI"
+                print self.m3u8_obj.base_uri
+                
+                for playlist in self.m3u8_obj.playlists:
+                    pth = self.m3u8_obj.base_uri + playlist.uri
+                        print "PLAYLIST URI: "
+                        print pth
+                        m3u8_playlist = m3u8.load(pth)
+                        for segment in m3u8_playlist.segments:
+                            key = str(playlist.stream_info.bandwidth)
+                                if not key in self.segments:
+                                    self.segments[key] = []
+                                self.segments[key].append(segment)
+                #print "KEYS"
+                #print self.segments.keys()
+                #print "Length"
+                #print len(self.segments)
+            self.index = 0
+    
+    def list_playlists(self):
+        print self.m3u8_obj.playlists
+        
+        
+        def list_bitrates(self):
+            res = []
+        #for key in self.segments.keys()
+        #	res.append(key)
+        #return res
+        
+        def next(self):
+            self.index += 1
+        #print(self.index)
+        
+        def get_segment(self, bitrate):
+            res = ""
+                
+                res += self.m3u8_obj.base_uri + self.segments[bitrate] [self.index].uri    + "\n"
+                res += self.m3u8_obj.base_uri + self.segments[bitrate] [self.index+1].uri  + "\n"
+                #next(self)
+                
+                return res
+        
+        
+        def dump(self):
+            print self.segments
 
-		print "HLSVOD Object"
-		print self.m3u8_obj
-
-		print "BASE URI"
-		print self.m3u8_obj.base_uri
-
-		for playlist in self.m3u8_obj.playlists:
-			pth = self.m3u8_obj.base_uri + playlist.uri
-			print "PLAYLIST URI: "
-			print pth
-			m3u8_playlist = m3u8.load(pth)
-			for segment in m3u8_playlist.segments:
-				key = str(playlist.stream_info.bandwidth)
-				if not key in self.segments:
-					self.segments[key] = [] 
-				self.segments[key].append(segment)
-				#print "KEYS"
-				#print self.segments.keys()
-				#print "Length"
-				#print len(self.segments)
-		self.index = 0
-
-	def list_playlists(self):
-		print self.m3u8_obj.playlists
-
-
-	def list_bitrates(self):
-		res = []
-		#for key in self.segments.keys()
-		#	res.append(key)
-		#return res
-
-	def next(self):
-		self.index += 1
-		#print(self.index)
-
-	def get_segment(self, bitrate):
-		res = ""
-
-		res += self.m3u8_obj.base_uri + self.segments[bitrate] [self.index].uri    + "\n"
-		res += self.m3u8_obj.base_uri + self.segments[bitrate] [self.index+1].uri  + "\n"
-		next(self)
-
-		return res
-	
-
-	def dump(self):
-		print self.segments
-
-		#for playlist in self.m3u8_obj.playlists:
-		#	print playlist.uri
+#for playlist in self.m3u8_obj.playlists:
+#	print playlist.uri
 
 
